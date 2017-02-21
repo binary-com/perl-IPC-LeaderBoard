20913196712a2cc23538e8fcc2b73d205f42a70e
Chylli <chylli.email@gmail.com>
2017-02-21 02:13:49 +0000

    v0.03

--- lib/IPC/LeaderBoard.pm
+++ lib/IPC/LeaderBoard.pm
@@ -10,7 +10,7 @@ use Moo;
 use Path::Tiny;
 use namespace::clean;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -373,8 +373,6 @@ sub update {
 
     return $operation_result;
 }
-
-
 
 =head1 AUTHOR
 
